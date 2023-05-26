73ab761662725007e4eb8bd06ce99869c4346e4a
Jacob Mayle <61399623+maylejacobj@users.noreply.github.com>
2023-05-26 10:08:48 -0400

    Update and rename Code.m to RelSerreCurve.m

--- RelSerreCurve.m
+++ RelSerreCurve.m
@@ -1,15 +1,22 @@
 /////////////////////////////////////////////////
-// Below we load the code of Zywina, Sutherland,
-// and Rouse--Zureick-Brown. The file
-// "2adicimage.txt" is modified to wrap the
-// relevant code in a function TwoAdic(E).
+// All of our code was run on a machine with the
+// following specificiations.
+// CPU: 2.9 GHz 6-Core Intel Core i9
+// Memory: 32 GB 2400 MHz DDR4
+// OS: macOS Montery Version 12.5.1
+// Magma Version: V2.27-3
 /////////////////////////////////////////////////
-ChangeDirectory("/Users/.../SurjectivityOfGalois");
-load "SurjectivityOfGalois.m";
-ChangeDirectory("/Users/.../galrep");
-load "algorithms.m";
-ChangeDirectory("/Users/.../2adic");
-load "2adicimage.txt";
+
+/////////////////////////////////////////////////
+// Below we load "ell-adic-galois-images" by 
+// Rouse--Sutherland--Zureick-Brown, which is
+// available online at 
+// https://github.com/AndrewVSutherland/ell-adic-galois-images.
+/////////////////////////////////////////////////
+ChangeDirectory("ell-adic-galois-images-main");
+Attach("groups/gl2.m");
+load "groups/gl2data.m";
+RSZB := GL2Load(0);
 
 /////////////////////////////////////////////////
 // Given a positive integer n, output GL(2,Z/nZ).
@@ -108,8 +115,10 @@ end function;
 // divisor of n such that among subgroups K of G:
 // [K(n),K(n)] = [G(n),G(n)] implies [K,K] = [G,G].
 //////////////  Used in Lemma 2.5 ///////////////
-// > LevelLower1(TwoB(72));
-// 36	// warning: this computation takes several hours
+// > LevelLower2(TwoB(72));
+// 36	// Warning: This computation is slow. It
+//      // took us about 2 hours to complete on
+//      // the marchine specified above.
 /////////////////////////////////////////////////
 LevelLower2 := function(G)
 	n := #BaseRing(G);
@@ -265,9 +274,9 @@ end function;
 // and S_{2Cn} defined in the introduction. 
 // See also Lemma 2.11.
 /////////////////////////////////////////////////
-S2Cs := {"H8","H8a","H8b","H8c","H8d","H38","H38a","H38b","H38c","H38d","H46","H46a","H46b","H46c","H46d"};
-S2B := {"H6","H14","H15","H16","H17","H18","H19"};
-S2Cn := {"H2","H2a","H2b"};
+S2Cs := {"2.6.0.1","8.12.0.2","4.12.0.2","8.12.0.1","4.12.0.1","8.12.0.3","8.24.0.5","8.24.0.7","8.24.0.2","8.24.0.1","8.12.0.4","8.24.0.6","8.24.0.8","8.24.0.3","8.24.0.4"};
+S2B := {"2.3.0.1","8.6.0.2","8.6.0.4","8.6.0.1","8.6.0.6","8.6.0.3","8.6.0.5"};
+S2Cn := {"2.2.0.1","4.4.0.2","8.4.0.1"};
 
 /////////////////////////////////////////////////
 // Given G = TwoCs(8), TwoB(4), or TwoCn(4),
@@ -282,41 +291,28 @@ S2Cn := {"H2","H2a","H2b"};
 // true
 /////////////////////////////////////////////////
 ConstructSG := function(G:GpOut:=false)
-	SG := [];
-	GpList := [];
-	m := #BaseRing(G);
+	AllGps := [x : x in RSZB | Characteristic(BaseRing(x`subgroup)) ne 0 and Characteristic(BaseRing(x`subgroup)) mod 2 eq 0];
+	GpsLbls := {};
+	Lbls := {};
+	m := Characteristic(BaseRing(G));
 	for H in FullCommDet(G) do
-		for S in finesublist do
-			gp := S[4];
-			lbl := "H" cat Substring(S[5],2,#S[5]);
-			n := #BaseRing(gp);
+		for i in [1..#AllGps] do 
+			gp := AllGps[i]`subgroup;
+			n := Characteristic(BaseRing(gp));
 			if n le m then
 				gp := gp @@ Red(m,n);
 				n := #BaseRing(gp);
 			end if;
 			if IsConjugate(Gl(m), Red(n, m)(gp), H) then
-				Include(~SG,lbl);
-				Include(~GpList,gp);
-			end if;
+				Include(~GpsLbls,<AllGps[i]`label,gp>);
+				Include(~Lbls,AllGps[i]`label);
+			end if;		
 		end for;
-		for i in [1..#newsublist] do
-    		gp := newsublist[i][3];
-    		lbl := "H" cat IntegerToString(i);
-    		n := #BaseRing(gp);
-    		if n le m then
-    			gp := gp @@ Red(m,n);
-    			n := #BaseRing(gp);
-    		end if;
-    		if IsConjugate(Gl(m), Red(n, m)(gp), H) then
-    			Include(~SG,lbl);
-    			Include(~GpList,gp);
-    		end if;
-    	end for;
 	end for;
 	if GpOut then
-    	return [<SG[i],GpList[i]> : i in [1..#SG]];
-    end if;
-    return SG;
+		return GpsLbls;
+	end if;
+	return Lbls;
 end function;
 
 /////////////////////////////////////////////////
@@ -325,33 +321,33 @@ end function;
 // for each subgroup H contained in the set S_G.
 ////////////// Used in Sec. 3 ///////////////////
 // > NumIdxTwoSubs(TwoCs(8));
-// H8 15
-// H8c 15
-// H38 15
-// H46 15
-// H8a 15
-// H8d 7
-// H8b 7
-// H46d 7
-// H38a 7
-// H46a 7
-// H46b 7
-// H38b 7
-// H38c 7
-// H46c 7
-// H38d 7
+// 8.24.0.5 7
+// 8.12.0.4 15
+// 8.24.0.7 7
+// 8.24.0.2 7
+// 8.12.0.3 15
+// 8.24.0.3 7
+// 8.12.0.1 15
+// 4.12.0.2 7
+// 8.24.0.1 7
+// 8.24.0.8 7
+// 8.24.0.6 7
+// 8.12.0.2 15
+// 8.24.0.4 7
+// 4.12.0.1 7
+// 2.6.0.1 15
 // > NumIdxTwoSubs(TwoB(4));
-// H6 7
-// H14 7
-// H15 7
-// H16 7
-// H17 7
-// H18 7
-// H19 7
+// 8.6.0.5 7
+// 8.6.0.2 7
+// 8.6.0.6 7
+// 8.6.0.1 7
+// 2.3.0.1 7
+// 8.6.0.3 7
+// 8.6.0.4 7
 // > NumIdxTwoSubs(TwoCn(4));
-// H2a 1
-// H2b 3
-// H2 3
+// 2.2.0.1 3
+// 8.4.0.1 3
+// 4.4.0.2 1
 /////////////////////////////////////////////////
 NumIdxTwoSubs := function(G)
     L := ConstructSG(G:GpOut:=true);
@@ -364,78 +360,21 @@ end function;
 
 /////////////////////////////////////////////////
 // Given an elliptic curve E/Q, output the set of
-// prime numbers ell such that the mod ell Galois
-// representation of E is non-surjective by
-// calling scripts of Zywina and Sutherland.
+// prime numbers ell such that the ell-adic Galois
+// representation of E is non-surjective.
 //////////////////// EXAMPLE ////////////////////
 // > E := EllipticCurve("66c3");
-// > NonsurjPrimes(E);
-// [ 2, 5 ]
+// > NonsurjPrimes(E,GL2EllAdicImages(E,RSZB));
+// { 2, 5 }
 /////////////////////////////////////////////////
-NonsurjPrimes := function(E)
-	L1 := [ell : ell in ExceptionalSet(E)];
-	L2 := PossiblyNonsurjectivePrimes(E,L1,1000);
-	L3 := [];
-	for ell in L2 do
-		if not "G" in GaloisImages(E,[ell],1000)[1] then
-			Append(~L3,ell);
-		end if;
+NonsurjPrimes := function(E,EllAdicImages)
+	Lbls := EllAdicImages;
+	NS := {};
+	for lbl in Lbls do
+		n := StringToInteger(Split(lbl,".")[1]);
+		Include(~NS,Factorization(n)[1][1]);
 	end for;
-	return L3;
-end function;
-
-/////////////////////////////////////////////////
-// Given an elliptic curve E/Q, output the image
-// of the 2-adic Galois representation of E by
-// calling a script of Rouse--Zureick-Brown.
-//////////////////// EXAMPLE ////////////////////
-// > E := EllipticCurve("66c3");
-// > TwoAdicGroup(E);
-// H15
-/////////////////////////////////////////////////
-TwoAdicGroup := function(E)
-	Curve := TwoAdic(E)[2];
-	return "H" cat Substring(Curve,2,#Curve);
-end function;
-
-/////////////////////////////////////////////////
-// Given an elliptic curve E/Q, output whether
-// the 3-adic Galois representation of E is 
-// surjective by implementing a result of Elkies.
-// It is sufficient to check surjectivity mod 9.
-//////////////////// EXAMPLES ///////////////////
-// > E := EllipticCurve("14a1"); // The mod 3 representation is nonsurjective
-// > ThreeAdicIsSurjective(E);
-// false
-// > E := EllipticCurve("62208j1"); // The mod 3 representation is surjective, but the mod 9 is nonsurjective
-// > ThreeAdicIsSurjective(E);
-// false 
-// > E := EllipticCurve("1944c1"); // The mod 3 representation is surjective, but the mod 9 is nonsurjective
-// > ThreeAdicIsSurjective(E);
-// false
-// > E := EllipticCurve("11a1"); // The 3-adic representation is surjective since the mod 9 is surjective 
-// > ThreeAdicIsSurjective(E);
-// true
-/////////////////////////////////////////////////
-ThreeAdicIsSurjective := function(E)
-    if not GaloisImages(E,[3],1000)[1] eq "3G" then
-        return false; // The mod 3 representation is nonsurjective
-    end if;
-    jinv := jInvariant(E);
-    if jinv eq 4374 then
-        return false; // The mod 3 representation is surjective, but the mod 9 is nonsurjective
-    end if;
-    S<t> := PolynomialRing(Rationals());
-    NumF := S ! 3^7*(t^2 -1)^3*(t^6 +3*t^5 +6*t^4 +t^3 -3*t^2 +12*t+16)^3*(2*t^3 +3*t^2 -3*t-5);
-    DenF := S ! (t^3-3*t-1)^9;
-    Numj := S ! Numerator(jinv);
-    Denj := S ! Denominator(jinv);
-    f := NumF*Denj - DenF*Numj;
-    R := Roots(f);
-    if #R gt 0 then
-        return false; // The mod 3 representation is surjective, but the mod 9 is nonsurjective
-    end if;
-    return true; // The 3-adic representation is surjective since the mod 9 is surjective
+	return NS;
 end function;
 
 /////////////////////////////////////////////////
@@ -461,17 +400,20 @@ end function;
 // false
 /////////////////////////////////////////////////
 IsRelSerreCurve := function(E)
-    if not HasComplexMultiplication(E) and #Exclude(NonsurjPrimes(E),2) eq 0 and ThreeAdicIsSurjective(E) then
-        Img := TwoAdicGroup(E);
-        if Img in S2Cs then
-            return <true,"2Cs">;
-        end if;
-        if Img in S2B then
-            return <true,"2B">;
-        end if;
-        if Img in S2Cn then
-            return <true,"2Cn">;
-        end if;
+    if not HasComplexMultiplication(E) then
+	    EllAdicImages := GL2EllAdicImages(E,RSZB);
+    	if NonsurjPrimes(E,EllAdicImages) eq {2} then
+			TwoAdicGroup := EllAdicImages[1];
+		    if TwoAdicGroup in S2Cs then
+	            return <true,"2Cs">;
+	        end if;
+	        if TwoAdicGroup in S2B then
+	            return <true,"2B">;
+	        end if;
+	        if TwoAdicGroup in S2Cn then
+	            return <true,"2Cn">;
+	        end if;
+		end if;
     end if;
     return false;
 end function;
@@ -531,7 +473,6 @@ end function;
 // > ImgCondCs(E);
 // 264
 /////////////////////////////////////////////////
-
 ImgCondCs := function(E)
 	E := WeierstrassModel(E);
 	C := Coefficients(Polynomial(E));
@@ -552,7 +493,8 @@ ImgCondCs := function(E)
 	BCsf := AbsoluteValue(Squarefree(B*C));
 	ABCsf := AbsoluteValue(Squarefree(A*B*C));
 	S := {Asf,Bsf,Csf,ABsf,ACsf,BCsf,ABCsf};
-	if TwoAdicGroup(E) in ["H8"] then
+	TwoAdicGroup := GL2EllAdicImages(E,RSZB)[1];
+	if TwoAdicGroup in ["2.6.0.1"] then
 		L := [x : x in MinWrtDiv(S,3)];
 		N1 := L[1];
 		N2 := L[2];
@@ -566,19 +508,19 @@ ImgCondCs := function(E)
 			return LCM([8,AbsoluteValue(NPrime(N1)),AbsoluteValue(NPrime(N2)),AbsoluteValue(NPrime(N3))]);
 		end if;
 	end if;
-	if TwoAdicGroup(E) in ["H8a","H8b","H8c","H8d","H38","H46"] then
+	if TwoAdicGroup in ["8.12.0.2","4.12.0.2","8.12.0.1","4.12.0.1","8.12.0.3","8.12.0.4"] then
 		L := [x : x in MinWrtDiv(S,2)];
 		N1 := L[1];
 		N2 := L[2];
 		N1Prime := NPrime(N1);
 		N2Prime := NPrime(N2);
-		if (N1*N2) mod 2 eq 0 or TwoAdicGroup(E) in ["H8a","H8c","H38","H46"] then
+		if (N1*N2) mod 2 eq 0 or TwoAdicGroup in ["8.12.0.2","8.12.0.1","8.12.0.3","8.12.0.4"] then
 			return LCM([8,AbsoluteValue(NPrime(N1)),AbsoluteValue(NPrime(N2))]);
 		else
 			return LCM([4,AbsoluteValue(NPrime(N1)),AbsoluteValue(NPrime(N2))]);
 		end if;
 	end if;
-	if TwoAdicGroup(E) in ["H38a","H38b","H38c","H38d","H46a","H46b","H46c","H46d"] then
+	if TwoAdicGroup in ["8.24.0.5","8.24.0.7","8.24.0.2","8.24.0.1","8.24.0.6","8.24.0.8","8.24.0.3","8.24.0.4"] then
 		L := [x : x in MinWrtDiv(S,1)];
 		N1 := L[1];
 		N1Prime := NPrime(N1);
@@ -616,7 +558,8 @@ ImgCondB := function(E)
 	Ysf := AbsoluteValue(Squarefree(Y));
 	XYsf := AbsoluteValue(Squarefree(X*Y));
 	S := {Xsf,Ysf,XYsf}; 
-	if TwoAdicGroup(E) eq "H6" then
+	TwoAdicGroup := GL2EllAdicImages(E,RSZB)[1];
+	if TwoAdicGroup eq "2.3.0.1" then
 		L := [x : x in MinWrtDiv(S,2)];
 		N1 := L[1];
 		N2 := L[2];
@@ -649,7 +592,8 @@ ImgCondCn := function(E)
 	K<a> := NumberField(DivisionPolynomial(E,2));
 	SDK := Integers() ! SquareRoot(Discriminant(RingOfIntegers(K)));
 	SDE := AbsoluteValue(Squarefree(Integers() ! SquareRoot(Discriminant(E))));
-	if TwoAdicGroup(E) eq "H2" then
+	TwoAdicGroup := GL2EllAdicImages(E,RSZB)[1];
+	if TwoAdicGroup eq "2.2.0.1" then
 		
 		if SDE mod 2 eq 0 then
 			return LCM([8,SDK,AbsoluteValue(NPrime(SDE))]);
@@ -658,10 +602,10 @@ ImgCondCn := function(E)
 			return LCM([4,SDK,AbsoluteValue(NPrime(SDE))]);
 		end if;
 	end if;
-	if TwoAdicGroup(E) eq "H2a" then
+	if TwoAdicGroup eq "4.4.0.2" then
 		return LCM(4,SDK);
 	end if;
-	if TwoAdicGroup(E) eq "H2b" then
+	if TwoAdicGroup eq "8.4.0.1" then
 		return LCM(8,SDK);
 	end if;
 end function;
@@ -716,31 +660,31 @@ end function;
 // Generate data for the table in the appendix 
 ////////////// Used for Appendix ////////////////
 // > GenerateTable();
-// <"2Cs", "H8", "315b2", [ 1, -1, 1, -68, 182 ], 420, "-">
-// <"2Cs", "H8a", "1800h2", [ 0, 0, 0, -3675, 35750 ], 120, "-">
-// <"2Cs", "H8b", "1089g2", [ 1, -1, 0, -12546, 173047 ], 132, "-">
-// <"2Cs", "H8c", "120b2", [ 0, 1, 0, -16, -16 ], 120, "-">
-// <"2Cs", "H8d", "33a1", [ 1, 1, 0, -11, 0 ], 132, "-">
-// <"2Cs", "H38", "350a2", [ 1, -1, 0, -442, -2784 ], 280, "-">
-// <"2Cs", "H38a", "392a2", [ 0, 0, 0, -931, -10290 ], 56, "-">
-// <"2Cs", "H38b", "3136s2", [ 0, 0, 0, -3724, 82320 ], 56, "-">
-// <"2Cs", "H38c", "112b2", [ 0, 0, 0, -19, -30 ], 56, "-">
-// <"2Cs", "H38d", "56a2", [ 0, 0, 0, -19, 30 ], 56, "-">
-// <"2Cs", "H46", "198a2", [ 1, -1, 0, -198, 1120 ], 264, "-">
-// <"2Cs", "H46a", "288b1", [ 0, 0, 0, -21, -20 ], 24, "-">
-// <"2Cs", "H46b", "576c2", [ 0, 0, 0, -84, -160 ], 24, "-">
-// <"2Cs", "H46c", "96a1", [ 0, 1, 0, -2, 0 ], 24, "-">
-// <"2Cs", "H46d", "66b2", [ 1, 1, 1, -22, -49 ], 264, "-">
-// <"2B", "H6", "69a2", [ 1, 0, 1, -16, -25 ], 276, 1>
-// <"2B", "H14", "1152o2", [ 0, 0, 0, -216, -864 ], 24, 1>
-// <"2B", "H15", "102a1", [ 1, 1, 0, -2, 0 ], 136, 78337/78336>
-// <"2B", "H16", "46a1", [ 1, -1, 0, -10, -12 ], 184, 267169/267168>
-// <"2B", "H17", "46a2", [ 1, -1, 0, -170, -812 ], 184, 1>
-// <"2B", "H18", "490g2", [ 1, 0, 0, -1191, 15721 ], 56, 1>
-// <"2B", "H19", "102a2", [ 1, 1, 0, 8, 10 ], 136, 1>
-// <"2Cn", "H2", "392f1", [ 0, 0, 0, -7, 7 ], 28, 2017/2016>
-// <"2Cn", "H2a", "392c1", [ 0, -1, 0, -16, 29 ], 28, 2017/2016>
-// <"2Cn", "H2b", "3136c1", [ 0, 0, 0, -1372, -19208 ], 56, 2017/2016>
+// <"2Cs", "2.6.0.1", "315b2", [ 1, -1, 1, -68, 182 ], 420, "-">
+// <"2Cs", "8.12.0.2", "1800h2", [ 0, 0, 0, -3675, 35750 ], 120, "-">
+// <"2Cs", "4.12.0.2", "1089g2", [ 1, -1, 0, -12546, 173047 ], 132, "-">
+// <"2Cs", "8.12.0.1", "120b2", [ 0, 1, 0, -16, -16 ], 120, "-">
+// <"2Cs", "4.12.0.1", "33a1", [ 1, 1, 0, -11, 0 ], 132, "-">
+// <"2Cs", "8.12.0.3", "350a2", [ 1, -1, 0, -442, -2784 ], 280, "-">
+// <"2Cs", "8.24.0.5", "392a2", [ 0, 0, 0, -931, -10290 ], 56, "-">
+// <"2Cs", "8.24.0.7", "3136s2", [ 0, 0, 0, -3724, 82320 ], 56, "-">
+// <"2Cs", "8.24.0.2", "112b2", [ 0, 0, 0, -19, -30 ], 56, "-">
+// <"2Cs", "8.24.0.1", "56a2", [ 0, 0, 0, -19, 30 ], 56, "-">
+// <"2Cs", "8.12.0.4", "198a2", [ 1, -1, 0, -198, 1120 ], 264, "-">
+// <"2Cs", "8.24.0.6", "288b1", [ 0, 0, 0, -21, -20 ], 24, "-">
+// <"2Cs", "8.24.0.8", "576c2", [ 0, 0, 0, -84, -160 ], 24, "-">
+// <"2Cs", "8.24.0.3", "96a1", [ 0, 1, 0, -2, 0 ], 24, "-">
+// <"2Cs", "8.24.0.4", "66b2", [ 1, 1, 1, -22, -49 ], 264, "-">
+// <"2B", "2.3.0.1", "69a2", [ 1, 0, 1, -16, -25 ], 276, 1>
+// <"2B", "8.6.0.2", "1152o2", [ 0, 0, 0, -216, -864 ], 24, 1>
+// <"2B", "8.6.0.4", "102a1", [ 1, 1, 0, -2, 0 ], 136, 78337/78336>
+// <"2B", "8.6.0.1", "46a1", [ 1, -1, 0, -10, -12 ], 184, 267169/267168>
+// <"2B", "8.6.0.6", "46a2", [ 1, -1, 0, -170, -812 ], 184, 1>
+// <"2B", "8.6.0.3", "490g2", [ 1, 0, 0, -1191, 15721 ], 56, 1>
+// <"2B", "8.6.0.5", "102a2", [ 1, 1, 0, 8, 10 ], 136, 1>
+// <"2Cn", "2.2.0.1", "392f1", [ 0, 0, 0, -7, 7 ], 28, 2017/2016>
+// <"2Cn", "4.4.0.2", "392c1", [ 0, -1, 0, -16, 29 ], 28, 2017/2016>
+// <"2Cn", "8.4.0.1", "3136c1", [ 0, 0, 0, -1372, -19208 ], 56, 2017/2016>
 /////////////////////////////////////////////////
 GenerateTable := function()
     L := [
@@ -771,14 +715,15 @@ GenerateTable := function()
     EllipticCurve("3136c1")];
     for E in L do
         GpTup := IsRelSerreCurve(E);
+        TwoAdicGroup := GL2EllAdicImages(E,RSZB)[1];
         if GpTup[2] eq "2Cs" then
-            Out := <GpTup[2], TwoAdicGroup(E), CremonaReference(E), Coefficients(E), ImgCondCs(E), "-">;
+            Out := <GpTup[2], TwoAdicGroup, CremonaReference(E), Coefficients(E), ImgCondCs(E), "-">;
         end if;
         if GpTup[2] eq "2B" then
-            Out := <GpTup[2], TwoAdicGroup(E), CremonaReference(E), Coefficients(E), ImgCondB(E), CyclicityCorrectionFactorB(E)>;
+            Out := <GpTup[2], TwoAdicGroup, CremonaReference(E), Coefficients(E), ImgCondB(E), CyclicityCorrectionFactorB(E)>;
         end if;
         if GpTup[2] eq "2Cn" then
-            Out := <GpTup[2], TwoAdicGroup(E), CremonaReference(E), Coefficients(E), ImgCondCn(E), CyclicityCorrectionFactorCn(E)>;
+            Out := <GpTup[2], TwoAdicGroup, CremonaReference(E), Coefficients(E), ImgCondCn(E), CyclicityCorrectionFactorCn(E)>;
         end if;
         Out;
     end for;
